7e7d30e1e4c487a3583de788a3725b8bf64f04ad
Namithia20 <yinaTsukita@gmail.com>
2018-11-27 11:20:14 +0100

    añadir paneles con plantilla, select editable y leer datos

--- src/main/java/com/mycompany/controllers/FXMLController.java
+++ src/main/java/com/mycompany/controllers/FXMLController.java
@@ -4,14 +4,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
 
 public class FXMLController implements Initializable {
     
@@ -20,9 +23,6 @@ public class FXMLController implements Initializable {
     
      @FXML
     private TitledPane panelD;
-
-    @FXML
-    private AnchorPane contenedor;
 
     @FXML
     private Pane PaneCopy;
@@ -36,27 +36,48 @@ public class FXMLController implements Initializable {
     @FXML
     private VBox cont;
     
+    @FXML
+    private ListView<String> Lista;    
+
+    @FXML
+    private Label lSelection;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO        
         CB_Test.getItems().addAll("A", "B", "C"); //asigna valores al desplegable
         cont.setSpacing(10);
-        
     }   
     
-    public void printValue()
+    
+    @FXML
+    public void deletePane(ActionEvent event) {
+        Pane p = (Pane)((Button)event.getSource()).getParent();
+        ((VBox)p.getParent()).getChildren().remove(p);
+    }
+    
+    @FXML
+    public void printValue(ActionEvent event) //genera nuevos paneles aunque se borren todos los visibles
     {
-        System.out.println((String) CB_Test.getValue()); //devuelve el valor seleccionado o escrito del combobox
-       
         Pane nuevo = new Pane();
-        //nuevo = new Pane(PaneCopy);
-        nuevo=clonaPanel(PaneCopy);
-        double val =  nuevo.getBoundsInParent().getHeight()*2;
-        nuevo.setLayoutY(val);
-         System.out.println("X = "+nuevo.getLayoutX()+", Y = "+nuevo.getLayoutY() + ", bounds = "+nuevo.getBoundsInParent().getHeight());
+        nuevo = clonaPanel(PaneCopy);
         cont.getChildren().add(nuevo);
-         System.out.println(cont.getChildren().size());
+    }   
+    
+    @FXML
+    void Leedatos(ActionEvent event) {
+        lSelection.setText("");
+        lSelection.setText(CB_Test.getValue());
+        
+        Lista.getItems().clear();
+        
+        for(Node child:cont.getChildren())
+        {
+            String t = ((TextField)((Pane) child).getChildren().get(2)).getText();
+            Lista.getItems().add(t);
+        }
+        
     }
     
     public Pane clonaPanel(Pane p)
@@ -65,7 +86,40 @@ public class FXMLController implements Initializable {
         
         clon.setLayoutX(p.getLayoutX());
         clon.setLayoutY(p.getLayoutY());
-        clon.styleProperty().set(p.getStyle());        
+        clon.setPrefHeight(p.getPrefHeight());
+        clon.setPrefWidth(p.getPrefWidth());
+        clon.getStyleClass().add("panelClon");
+        
+        Label l = new Label();
+        Label lc =(Label) p.getChildren().get(0);
+        
+        l.setLayoutX(lc.getLayoutX());
+        l.setLayoutY(lc.getLayoutY());
+        l.setPrefHeight(lc.getPrefHeight());
+        l.setPrefWidth(lc.getPrefWidth());
+        l.setText(lc.getText());
+        l.getStyleClass().add("Mytext");
+        
+        Button b =  new Button();
+        Button bc = (Button) p.getChildren().get(1);
+        
+        b.setLayoutX(bc.getLayoutX());
+        b.setLayoutY(bc.getLayoutY());
+        b.setPrefHeight(bc.getPrefHeight());
+        b.setPrefWidth(bc.getPrefWidth());
+        b.setText(bc.getText());
+        b.setOnAction(bc.getOnAction()); //copia la función deletePane del origen
+        
+        TextField t = new TextField();
+        TextField tc = (TextField) p.getChildren().get(2);
+        
+        t.setLayoutX(tc.getLayoutX());
+        t.setLayoutY(tc.getLayoutY());
+        t.setPrefHeight(tc.getPrefHeight());
+        t.setPrefWidth(tc.getPrefWidth());
+        
+        
+        clon.getChildren().addAll(l, b, t);
         
         return clon;
     }
